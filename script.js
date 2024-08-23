fdee33df49d6965211588f9f197d7d66fe4eda42
lrayne <legaterayne@gmail.com>
2024-08-23 18:11:43 +0300

    feat: Update .css

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(() => {
+      var _animate = document.querySelectorAll(".animate");
+      _animate.forEach(function(element, index) {
+        const _delay = index * 300;
+        setTimeout(() => {
+          element.classList.add("loaded");
+        }, _delay);
+      });
+    }, 500);
+  });
+  
